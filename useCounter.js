c022a5794df6969721f2606f98aa49c3bd68e044
Armin <soporte@wera.com.mx>
2022-04-18 17:09:18 -0600

    Primer commit

---
+++ useCounter.js
@@ -0,0 +1,27 @@
+import { getSpaceUntilMaxLength } from '@testing-library/user-event/dist/utils';
+import {useState} from 'react'
+
+export const useCounter = (initialState = 10) => {
+    
+    const [counter, setCounter] = useState(initialState);
+
+    const increment = () =>{
+        setCounter(counter + 1);
+    }
+
+    const decrement = () => {
+        setCounter(counter - 1);
+    }
+
+    const reset = () => {
+        setCounter(initialState);
+    }
+
+    return{
+        counter,
+        increment,
+        decrement,
+        reset
+    }
+
+}
